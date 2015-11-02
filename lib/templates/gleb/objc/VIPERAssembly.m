af85522c6f8e21315b295fb6c334888d3bab6a85
gtarasov <gleb34@gmail.com>
2015-11-03 02:59:36 +0300

    fixes

--- lib/templates/gleb/objc/VIPERAssembly.m
+++ lib/templates/gleb/objc/VIPERAssembly.m
@@ -15,7 +15,7 @@
 
 @implementation VIPERAssembly
 
-- (Module *)newsModule {
+- (Module *)viperModule {
     return [TyphoonDefinition withClass:Module.class configuration:^(TyphoonDefinition *definition) {
         [definition injectProperty:@selector(interactor) with:self.viperInteractor];
         [definition injectProperty:@selector(viewController) with:self.viperViewController];
@@ -31,13 +31,15 @@
                                    [factoryMethod injectParameterWith:NSStringFromClass(VIPERViewController.class)];
                                } configuration:^(TyphoonFactoryDefinition *definition) {
                                    [definition injectProperty:@selector(presenter) with:self.viperPresenter];
+                                   
                                    definition.scope = TyphoonScopeObjectGraph;
                                }];
 }
 
 - (id<VIPERPresenterProtocol>)viperPresenter {
     return [TyphoonDefinition withClass:VIPERPresenter.class configuration:^(TyphoonDefinition *definition) {
-        [definition injectProperty:@selector(wireframe)];
+        [definition injectProperty:@selector(wireframe)
+                              with:self.viperWireframe];
         
         [definition injectProperty:@selector(viewController)
                               with:self.viperViewController];
@@ -54,7 +56,9 @@
 - (id<VIPERWireframeProtocol>)viperWireframe {
     return [TyphoonDefinition withClass:VIPERWireframe.class configuration:^(TyphoonDefinition *definition) {
         [definition injectProperty:@selector(rootRouter)];
-        [definition injectProperty:@selector(presenter) with:self.viperPresenter];
+        
+        [definition injectProperty:@selector(presenter)
+                              with:self.viperPresenter];
     }];
 }
 
